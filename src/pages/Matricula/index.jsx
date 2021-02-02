d66c1689e825ac8989c847f9740688e2737a80ec
marcosoliveira78 <marcsantos@hotmail.com>
2021-02-02 18:24:40 -0300

    Cidade e Estado

--- src/pages/Matricula/index.jsx
+++ src/pages/Matricula/index.jsx
@@ -5,8 +5,9 @@ import React, { useEffect, useState } from 'react';
 import { Col, Form, Jumbotron } from 'react-bootstrap';
 import { Link, useHistory } from 'react-router-dom';
 import uniqueId from 'lodash';
-// import cidades from 'cidades-estados-brasil-json';
 import axios from 'axios';
+import estados from '../../repositories/estados.json';
+import municipios from '../../repositories/municipios.json';
 import request from '../../services/api';
 import FormSelect from '../../component/FormSelect';
 import FormField from '../../component/FormField';
@@ -16,15 +17,18 @@ import {
 import { Label } from '../../component/FormSelect/styles';
 
 const Matricula = () => {
-  const valoresInciais = { codigoGenero: 2 };
   const history = useHistory();
   const isValid = true;
   let nextId;
   const pasta = 'matriculas';
 
   const [matricula, setMatricula] = useState([]);
-  const [genero, setGenero] = useState([]);
-  const [naturalidade] = useState(['Leopoldina', 'Cataguases', 'Ubá', 'Muriaé']);
+  const [estadosOptions, setEstadosOptions] = useState([]);
+  const [cidadesOptions, setCidadesOptions] = useState([]);
+  const [cidades, setCidades] = useState([]);
+
+  // const [genero, setGenero] = useState([]);
+  // const [naturalidade] = useState(['Leopoldina', 'Cataguases', 'Ubá', 'Muriaé']);
 
   // $get.cidades;
 
@@ -33,54 +37,113 @@ const Matricula = () => {
     setMatricula({ ...matricula, [name]: value });
   };
 
+  const handleChangeNaturalidadeUF = (event) => {
+    const { value, label } = event;
+    setMatricula({ ...matricula, naturalidadeUF: label, codigoNaturalidadeUF: value });
+  };
+
   const handleChangeNaturalidade = (event) => {
-    const { value, label } = event.target;
+    const { value, label } = event;
     setMatricula({ ...matricula, naturalidade: label, codigoNaturalidade: value });
-    // setMatricula({ ...matricula, codigoNaturalidade: value });
-    // getSwitchNaturalidade(value);
   };
 
   const handleChangeGenero = (event) => {
-    const { value, label } = event.target;
+    const { value } = event.target;
+    const label = event.target.labels[0].textContent;
     setMatricula({ ...matricula, codigoGenero: value, genero: label });
-    setGenero(value);
+    // setGenero(value);
+  };
+
+  const handleChangeNacionalidade = (event) => {
+    const { value } = event.target;
+    const label = event.target.labels[0].textContent;
+    setMatricula({ ...matricula, codigoNacionalidade: value, nacionalidade: label });
+    // setGenero(value);
   };
 
   const handleSubmit = async (form) => {
     form.preventDefault();
     console.log('NOME: ', matricula.nome);
-    axios.post('http://localhost:8080/matriculas', {
-      id: nextId,
-      nome: matricula.nome,
-      dataNascimento: '30/03/1978',
-      email: 'marcsantos@hotmail.com',
-    }).then((resp) => {
-      console.log(resp.data);
-      history.push('/listagemGeral');
-    }).catch((error) => {
-      console.log(error);
-    });
+    // axios.post('http://localhost:8080/matriculas', {
+    //   id: nextId,
+    //   nome: matricula.nome,
+    //   dataNascimento: '30/03/1978',
+    //   email: 'marcsantos@hotmail.com',
+    // }).then((resp) => {
+    //   console.log(resp.data);
+    //   history.push('/listagemGeral');
+    // }).catch((error) => {
+    //   console.log(error);
+    // });
+    fetch(`http://localhost:8080/${pasta}`,
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        mode: 'cors',
+        cache: 'default',
+        body: JSON.stringify({
+          id: nextId,
+          nome: matricula.nome,
+          dataNascimento: matricula.dataNascimento,
+          email: 'marcsantos@hotmail.com',
+        }),
+      })
+      .then(async (resp) => {
+        console.log(resp);
+      // const resultado = await resp.json();
+      // const lastKey = Object.keys(resultado).reverse()[0];
+      // nextId = resultado[lastKey].id + 1;
+      // console.log('ID: ', nextId);
+      });
   };
 
   useEffect(() => {
-    request('get', `http://localhost:8080/${pasta}`)
+    // request('get', `http://localhost:8080/${pasta}`)
+    fetch(`http://localhost:8080/${pasta}`,
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        mode: 'cors',
+        cache: 'default',
+      })
       .then(async (resp) => {
-        const resultado = await resp;
+        const resultado = await resp.json();
         const lastKey = Object.keys(resultado).reverse()[0];
         nextId = resultado[lastKey].id + 1;
         console.log('ID: ', nextId);
       });
+
+    setEstadosOptions(estados.map((estado) => (
+      {
+        value: estado.id,
+        label: `${estado.nome} (${estado.sigla})`,
+      }
+    )));
   }, []);
 
-  let cidadeId = 1;
   // Options
-  const naturalidadeOptions = naturalidade.map((cidade) => (
-    {
-      value: cidadeId++,
-      label: cidade,
-    }
-  ));
+  useEffect(() => {
+    if (matricula.naturalidadeUF !== undefined) {
+      setCidades(municipios.map((c) => c));
+      const municipiosFiltrados = cidades.filter((cidade) => (
+        cidade.microrregiao.mesorregiao.UF.id.toString().includes(matricula.codigoNaturalidadeUF)));
 
+      setCidadesOptions(municipiosFiltrados.map((cidade) => (
+        {
+          value: cidade.id,
+          label: `${cidade.nome} - ${cidade.microrregiao.mesorregiao.UF.sigla}`,
+        }
+      )));
+    }
+  }, [matricula.codigoNaturalidadeUF]);
+
+  // console.log('Cidades: ', cidadesOptions);
   // const response = await itemRep.save([item]);
   // if (response !== null && response.success) {
   // }
@@ -97,14 +160,7 @@ const Matricula = () => {
 
         <Wrapper>
           <Form onSubmit={handleSubmit}>
-            {/* <FormSelect
-              label="Grupo:"
-              name="codigoGrupo"
-              value={item.codigoGrupo}
-              onChange={handleChangeGrupo}
-              options={groupOptions}
-            />
-            {errors.codigoGrupo && <MessageError>{errors.codigoGrupo}</MessageError>} */}
+            {/* {errors.codigoGrupo && <MessageError>{errors.codigoGrupo}</MessageError>}  */}
             <FormField
               label="Nome do Aluno"
               name="nome"
@@ -118,7 +174,7 @@ const Matricula = () => {
               name="cpf"
               type="text"
               value={matricula.cpf}
-              maxLength={13}
+              maxLength={14}
               onChange={handleChange}
             />
             <FormField
@@ -129,26 +185,19 @@ const Matricula = () => {
               maxLength={10}
               onChange={handleChange}
             />
-            <FormSelect
-              label="Naturalidade:"
-              name="naturalidade"
-              value={matricula.codigoNaturalidade}
-              onChange={handleChangeNaturalidade}
-              options={naturalidadeOptions}
-            />
-            <fieldset>
+            <fieldset style={{ marginTop: '15px' }}>
               <ContainerAlignLeft className="TipoContainer">
-                <Label style={{ fontSize: '18px', margin: 0 }}>
-                  Tipo:
+                <Label style={{ fontSize: '18px', margin: '0 0 0 12px' }}>
+                  Gênero:
                 </Label>
                 <Col sm={10} className="column">
                   <Form.Check
                     inline
                     label="Masculino"
                     type="radio"
-                    checked={genero === '1'}
+                    checked={matricula.codigoGenero === '1'}
                     name="genero"
-                    id="inline-radio-1"
+                    id="inline-genero-1"
                     value="1"
                     onChange={handleChangeGenero}
                   />
@@ -156,9 +205,9 @@ const Matricula = () => {
                     inline
                     label="Feminino"
                     type="radio"
-                    checked={genero === '2'}
+                    checked={matricula.codigoGenero === '2'}
                     name="genero"
-                    id="inline-radio-2"
+                    id="inline-genero-2"
                     value="2"
                     onChange={handleChangeGenero}
                   />
@@ -166,9 +215,9 @@ const Matricula = () => {
                     inline
                     label="Outros"
                     type="radio"
-                    checked={genero === '3'}
+                    checked={matricula.codigoGenero === '3'}
                     name="genero"
-                    id="inline-radio-3"
+                    id="inline-genero-3"
                     value="3"
                     onChange={handleChangeGenero}
                   />
@@ -176,11 +225,55 @@ const Matricula = () => {
               </ContainerAlignLeft>
               {/* {errors.tipoSolicitacao && <MessageError>{errors.tipoSolicitacao}</MessageError>} */}
             </fieldset>
+            <fieldset style={{ marginTop: '15px' }}>
+              <ContainerAlignLeft className="TipoContainer">
+                <Label style={{ fontSize: '18px', margin: '0 0 0 12px' }}>
+                  Nacionalidade:
+                </Label>
+                <Col sm={10} className="column">
+                  <Form.Check
+                    inline
+                    label="Brasileira"
+                    type="radio"
+                    checked={matricula.codigoNacionalidade === '1'}
+                    name="nacionalidade"
+                    id="inline-nacionalidade-1"
+                    value="1"
+                    onChange={handleChangeNacionalidade}
+                  />
+                  <Form.Check
+                    inline
+                    label="Estrangeira"
+                    type="radio"
+                    checked={matricula.codigoNacionalidade === '2'}
+                    name="nacionalidade"
+                    id="inline-nacionalidade-2"
+                    value="2"
+                    onChange={handleChangeNacionalidade}
+                  />
+                </Col>
+              </ContainerAlignLeft>
+              {/* {errors.tipoSolicitacao && <MessageError>{errors.tipoSolicitacao}</MessageError>} */}
+            </fieldset>
+            <FormSelect
+              label="Naturalidade: (Estado de Origem)"
+              name="naturalidadUF"
+              value={matricula.codigoNaturalidadeUF}
+              onChange={handleChangeNaturalidadeUF}
+              options={estadosOptions}
+            />
+            <FormSelect
+              label="Naturalidade: (Cidade de Origem)"
+              name="naturalidade"
+              value={matricula.codigoNaturalidade}
+              onChange={handleChangeNaturalidade}
+              options={cidadesOptions}
+            />
             <FormField
-              label="Nome do Aluno"
-              name="nome"
+              label="E-mail"
+              name="email"
               type="text"
-              value={matricula.nome}
+              value={matricula.email}
               maxLength={200}
               onChange={handleChange}
             />
