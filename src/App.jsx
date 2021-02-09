6ce6d41ba4b08bde8c3b38044325121b8bf98c8b
marcosoliveira78 <marcsantos@hotmail.com>
2021-02-09 19:32:28 -0300

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PageDefault from './component/PageDefault';
 import LoginProvider from './context/Login';
-import MatriculaProvider from './context/Matricula';
+import MatriculaProvider from './context/Matricula/index';
 import Routes from './services/routes';
 
 const App = () => (
