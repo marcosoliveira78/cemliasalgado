6c177f9dca6137b090ec5be950ba0c25cea27770
marcosoliveira78 <marcsantos@hotmail.com>
2021-02-09 19:35:23 -0300

    acerto de context

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import PageDefault from './component/PageDefault';
 import LoginProvider from './context/Login';
-import MatriculaProvider from './context/Matricula/index';
+import MatriculaProvider from './context/Matricula';
 import Routes from './services/routes';
 
 const App = () => (
-      <LoginProvider>
   <MatriculaProvider>
-    <PageDefault>
-      <Routes />
-    </PageDefault>
+    <LoginProvider>
+      <PageDefault>
+        <Routes />
+      </PageDefault>
+    </LoginProvider>
   </MatriculaProvider>
-      </LoginProvider>
 );
 
 export default App;
